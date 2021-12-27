869a4853d098e478a54b9fa2a7224dac871dd78e
Michael Li <thisislimichael@gmail.com>
2021-12-27 01:35:54 -0800

    added pw verification

--- routes/api/user-routes.js
+++ routes/api/user-routes.js
@@ -51,8 +51,17 @@ router.post('/', (req, res) => {
 });
 
 // PUT /api/users/1
-router.put('/:id', (req, res) => {});
-
+router.put('/:id', (req, res) => {
+    // expects {username: 'Lernantino', email: 'lernantino@gmail.com', password: 'password1234'}
+  
+    // pass in req.body instead to only update what's passed through
+    User.update(req.body, {
+      individualHooks: true,
+      where: {
+        id: req.params.id
+      }
+    })
+});
 // DELETE /api/users/1
 router.delete('/:id', (req, res) => {});
 
