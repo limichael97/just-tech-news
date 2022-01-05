471994dcdc27988ee0d94812dff38d015dee3e8a
Michael Li <thisislimichael@gmail.com>
2022-01-04 20:56:22 -0800

    added helpers

--- server.js
+++ server.js
@@ -1,4 +1,6 @@
 const path = require('path');
+const helpers = require('./utils/helpers');
+
 const express = require('express');
 const routes = require('./controllers');
 const sequelize = require('./config/connection');
@@ -6,7 +8,7 @@ const session = require('express-session');
 
 const SequelizeStore = require('connect-session-sequelize')(session.Store);
 const exphbs = require('express-handlebars');
-const hbs = exphbs.create({});
+const hbs = exphbs.create({helpers });
 const sess = {
   secret: 'Super secret secret',
   cookie: {},
